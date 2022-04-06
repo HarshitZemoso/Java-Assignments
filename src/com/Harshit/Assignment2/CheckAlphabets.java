69b323f2aa4ff64ebb657c6b0c8ffd64f39ce4b1
Harshit Sachdeva <harshit.sachdeva@zemosolabs.com>
2022-04-06 22:54:35 +0530

    Java Assignmets

---
+++ src/com/Harshit/Assignment2/CheckAlphabets.java
@@ -0,0 +1,29 @@
+package com.Harshit.Assignment2;
+
+import java.util.Scanner;
+
+public class CheckAlphabets {
+    public static boolean checkAll(String s){
+
+        int[] alphabets = new int[26];
+        for (int i = 0; i < s.length() ; i++) {
+            alphabets[s.charAt(i)-'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (alphabets[i]==0)
+                    return false;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        String s = sc.next();
+        boolean ans = checkAll(s.toLowerCase());
+        if (ans)
+            System.out.println("String contains all the alphabets");
+        else
+            System.out.println("String Does Not Contain all the alphabets!");
+    }
+
+
+}
---
+++ src/com/Harshit/Main.java
@@ -0,0 +1,8 @@
+package com.Harshit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
